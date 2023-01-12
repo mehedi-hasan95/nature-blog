5903b5fc059d2c0732e99bb89acc0acaac194887
mehedititu <mh.me.mehedi@gmail.com>
2023-01-12 21:27:01 +0600

    Update Post Done

--- src/Pages/Singlepage/Singlepage.jsx
+++ src/Pages/Singlepage/Singlepage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { Link, useLoaderData, useNavigate } from "react-router-dom";
@@ -8,6 +8,9 @@ import Sidebar from "../Home/Sidebar/Sidebar";
 const Singlepage = () => {
     const { user } = useContext(authContext);
     const navigate = useNavigate();
+
+    const [updateMode, setUpdateMode] = useState(false);
+
     const blogImage = "http://localhost:5000/images/";
     const { title, desc, createdAt, username, photo, _id } = useLoaderData();
 
@@ -29,6 +32,31 @@ const Singlepage = () => {
                 });
         } catch (error) {}
     };
+
+    // Update Post
+    const [updateTitle, setUpdateTitle] = useState("");
+    const [updateDesc, setUpdateDesc] = useState("");
+    const updatePost = () => {
+        const data = {
+            username: user.username,
+            title: updateTitle,
+            desc: updateDesc,
+        };
+
+        fetch(`http://localhost:5000/api/post/${_id}`, {
+            method: "PUT", // or 'PUT'
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                navigate("/");
+                console.log("Success:", data);
+            });
+    };
+
     return (
         <div>
             <div className="grid grid-cols-4 gap-6 md:gap-12 lg:gap-20 pt-6 rounded-md shadow-sm relative">
@@ -43,15 +71,34 @@ const Singlepage = () => {
                     <div>
                         <div className="items-center">
                             <h2 className="text-center font-lora font-semibold text-2xl">
-                                {title}
-                                {user?.username === username && (
-                                    <div className="flex gap-5 lg:float-right">
-                                        <FaEdit className="cursor-pointer text-green-700" />
-                                        <MdDelete
-                                            onClick={deletePost}
-                                            className="cursor-pointer text-red-800"
-                                        />
-                                    </div>
+                                {updateMode ? (
+                                    <input
+                                        onChange={(e) =>
+                                            setUpdateTitle(e.target.value)
+                                        }
+                                        type="text"
+                                        id=""
+                                        defaultValue={title}
+                                        className="w-full border text-center p-2 outline-none border-violet-400 rounded-md"
+                                    />
+                                ) : (
+                                    <>
+                                        {title}
+                                        {user?.username === username && (
+                                            <div className="flex gap-5 lg:float-right">
+                                                <FaEdit
+                                                    onClick={() =>
+                                                        setUpdateMode(true)
+                                                    }
+                                                    className="cursor-pointer text-green-700"
+                                                />
+                                                <MdDelete
+                                                    onClick={deletePost}
+                                                    className="cursor-pointer text-red-800"
+                                                />
+                                            </div>
+                                        )}
+                                    </>
                                 )}
                             </h2>
                         </div>
@@ -68,8 +115,36 @@ const Singlepage = () => {
                             <p>{new Date(createdAt).toDateString()}</p>
                         </div>
                         <p className="pt-10 first-letter:text-3xl first-letter:font-extrabold first-letter:pl-5 leading-7 ">
-                            {desc}
+                            {updateMode ? (
+                                <textarea
+                                    onChange={(e) =>
+                                        setUpdateDesc(e.target.value)
+                                    }
+                                    id=""
+                                    rows="8"
+                                    defaultValue={desc}
+                                    className="w-full p-4  border outline-none border-violet-400 rounded-md"
+                                ></textarea>
+                            ) : (
+                                <>{desc}</>
+                            )}
                         </p>
+                        {updateMode && (
+                            <>
+                                <button
+                                    onClick={() => setUpdateMode(false)}
+                                    className="bg-purple-400 px-4 py-2 rounded-md mr-5"
+                                >
+                                    Close
+                                </button>
+                                <button
+                                    onClick={updatePost}
+                                    className="bg-purple-400 px-4 py-2 rounded-md"
+                                >
+                                    Update
+                                </button>
+                            </>
+                        )}
                     </div>
                 </div>
                 <div className="space-y-2 col-span-full md:col-span-1">
